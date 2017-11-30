74eba22f7113543a0a7a59b0681e9724741e553a
seoil <seoil@HH311-39>
2017-11-30 11:15:52 +0900

    PushPrint.java modified on Local repository

--- Hello World/src/PushPrint.java
+++ Hello World/src/PushPrint.java
@@ -4,6 +4,7 @@ public class PushPrint {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Add java class File Push");
+		System.out.println("world with Amugae");
 	}
 
 }
